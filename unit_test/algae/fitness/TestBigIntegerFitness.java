10a7c93ea27d968eb0155ab41bf7a6e768df971d
baz bishop <bazbishop@gmail.com>
2021-01-16 12:48:44 +0100

    Added test cases for IntegerFitness and BigIntegerFitness.

---
+++ unit_test/algae/fitness/TestBigIntegerFitness.java
@@ -0,0 +1,43 @@
+package algae.fitness;
+
+import java.math.BigInteger;
+
+import junit.framework.TestCase;
+
+public class TestBigIntegerFitness extends TestCase {
+	public void testIsOptimal() {
+		var notFinished = new BigIntegerFitness(BigInteger.valueOf(0), false);
+		assertEquals(notFinished.isOptimal(), false);
+
+		var finished = new BigIntegerFitness(BigInteger.valueOf(0), true);
+		assertEquals(finished.isOptimal(), true);
+	}
+
+	public void testCompareTo() {
+		var less = new BigIntegerFitness(BigInteger.valueOf(-5), false);
+
+		var middle1 = new BigIntegerFitness(BigInteger.valueOf(2), false);
+		var middle2 = new BigIntegerFitness(BigInteger.valueOf(2), false);
+
+		var more = new BigIntegerFitness(BigInteger.valueOf(177), false);
+
+		assertTrue(less.compareTo(middle1) < 0);
+		assertTrue(middle1.compareTo(middle1) == 0);
+		assertTrue(middle1.compareTo(middle2) == 0);
+		assertTrue(middle2.compareTo(more) < 0);
+
+		assertTrue(middle1.compareTo(less) > 0);
+		assertTrue(middle2.compareTo(middle1) == 0);
+		assertTrue(more.compareTo(middle2) > 0);
+	}
+
+	public void testAttributes() {
+		BigIntegerFitness a = new BigIntegerFitness(BigInteger.valueOf(17), false);
+		assertEquals(a.mValue, BigInteger.valueOf(17));
+		assertEquals(a.mFinished, false);
+
+		BigIntegerFitness b = new BigIntegerFitness(BigInteger.valueOf(-1), true);
+		assertEquals(b.mValue, BigInteger.valueOf(-1));
+		assertEquals(b.mFinished, true);
+	}
+}
