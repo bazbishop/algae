3c64bf01f83bff3a0a4e24783c2eb52054a65899
baz bishop <bazbishop@gmail.com>
2021-01-17 22:38:50 +0100

    Tidied up functional test case PropositionalSatisfiability.

--- functional_test/baz/sat/Disjunction.java
+++ functional_test/baz/sat/Disjunction.java
@@ -8,38 +8,33 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Disjunction
-{
-	public void add( Term term )
-	{
-		mTerms.add( term );
+public class Disjunction {
+	public void add(Term term) {
+		mTerms.add(term);
 	}
-	
-	public Set<String> getVariables()
-	{
+
+	public Set<String> getVariables() {
 		Set<String> result = new HashSet<String>();
-		for( Term term : mTerms )
-			result.add( term.variableName );
-		
+		for (Term term : mTerms)
+			result.add(term.variableName);
+
 		return result;
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		StringBuilder result = new StringBuilder();
-		result.append( '[' );
-		
+		result.append('[');
+
 		boolean first = true;
-		for( Term term : mTerms )
-		{
-			if( first )
+		for (Term term : mTerms) {
+			if (first)
 				first = false;
 			else
-				result.append( ',' );
-			result.append( term );
+				result.append(',');
+			result.append(term);
 		}
-		result.append( ']' );
-		
+		result.append(']');
+
 		return result.toString();
 	}
 
--- functional_test/baz/sat/FormulaFactory.java
+++ functional_test/baz/sat/FormulaFactory.java
@@ -2,86 +2,78 @@ package baz.sat;
 
 import java.util.Random;
 
-public class FormulaFactory
-{
-	public FormulaFactory( int numVariables, int literalsPerClause, int numClauses) {
+public class FormulaFactory {
+	public FormulaFactory(int numVariables, int literalsPerClause, int numClauses) {
 		mRandom = new Random();
-		
+
 		mNumVariables = numVariables;
 		mLiteralsPerClause = literalsPerClause;
 		mNumClauses = numClauses;
 	}
-	
-	public String makeFormula()
-	{
-		StringBuilder result = new StringBuilder();
-		
 
-		for( int clause = 0; clause < mNumClauses; ++clause )
-		{
-			result.append( "[" );
-			
+	public String makeFormula() {
+		StringBuilder result = new StringBuilder();
+
+		for (int clause = 0; clause < mNumClauses; ++clause) {
+			result.append("[");
+
 			int previousVarIndex = -1;
-			
+
 			boolean first = true;
-			for( int term = 0; term < mLiteralsPerClause; ++term )
-			{
-				if( first )
+			for (int term = 0; term < mLiteralsPerClause; ++term) {
+				if (first)
 					first = false;
 				else
-					result.append( ',' );
-				
-				if( mRandom.nextInt( 2 ) == 0 )
-					result.append( "!" );
-				
-				int varIndex; 
-				do
-				{
-					varIndex = mRandom.nextInt( mNumVariables );
-				} while( varIndex == previousVarIndex );
+					result.append(',');
 
-				result.append( makeVar( varIndex ) );
-				
+				if (mRandom.nextInt(2) == 0)
+					result.append("!");
+
+				int varIndex;
+				do {
+					varIndex = mRandom.nextInt(mNumVariables);
+				} while (varIndex == previousVarIndex);
+
+				result.append(makeVar(varIndex));
+
 				previousVarIndex = varIndex;
 			}
-			
-			result.append( "]" );
+
+			result.append("]");
 		}
-		
+
 		return result.toString();
 	}
 
-	private String makeVar( int n )
-	{
+	private String makeVar(int n) {
 		StringBuilder result = new StringBuilder();
-		
+
 		final int range = 26;
-		
+
 		int d1 = n % range;
 		n /= range;
 		int d2 = n % range;
 		n /= range;
 		int d3 = n % range;
-		
-		if( d3 > 0 )
-			result.append( toChar( 'A' + d3 ) );
-		
-		if( d2 > 0 || d3 > 0)
-			result.append( toChar( 'A' + d2 ) );
 
-		result.append( toChar( 'A' + d1 ) );
+		if (d3 > 0)
+			result.append(toChar('A' + d3));
+
+		if (d2 > 0 || d3 > 0)
+			result.append(toChar('A' + d2));
+
+		result.append(toChar('A' + d1));
 
 		return result.toString();
 	}
-	
-	private char toChar( int ch )
-	{
+
+	private char toChar(int ch) {
 		return (char) ch;
 	}
 
 	private final int mNumVariables;
 	private final int mLiteralsPerClause;
 	private final int mNumClauses;
-	
+
 	private final Random mRandom;
 }
