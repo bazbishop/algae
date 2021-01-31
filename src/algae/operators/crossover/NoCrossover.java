2c3a86fb2a4b80cba09046b95033441d7822f86f
baz bishop <bazbishop@gmail.com>
2021-01-31 14:45:21 +0100

    Added NoCrossover operator.

---
+++ src/algae/operators/crossover/NoCrossover.java
@@ -0,0 +1,25 @@
+package algae.operators.crossover;
+
+import algae.IChromosome;
+import algae.IChromosomeFactory;
+import algae.ICrossoverOperator;
+
+/**
+ * Null crossover operator that can only work with one parent and simply returns a copy of the paranet chromosome.
+ */
+public class NoCrossover implements ICrossoverOperator {
+
+	/**
+	 * Constructor.
+	 */
+	public NoCrossover() {
+	}
+
+	@Override
+	public IChromosome apply(IChromosome[] input, IChromosomeFactory factory) {
+
+		assert input.length == 1;
+		
+		return input[0];
+	}
+}
